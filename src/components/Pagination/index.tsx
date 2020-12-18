4e096e35d4fdf53ef2a7b709b7eaa32103bd4015
viniciu21 <viniciusoliveiras12@hotmail.com.br>
2020-12-18 07:49:17 -0300

    component Pagination

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Grid, Button } from "@material-ui/core";
+import { updatePagination } from "../../store/actions/pokedex";
+import { ApplicationState } from "../../store";
+
+const Pagination = () => {
+	const {pages} = useSelector<ApplicationState, ApplicationState["pokedexReducer"]>((state) => state.pokedexReducer);
+
+	const dispatch = useDispatch();
+
+	const goPrevious = () => {
+		const prevPage = pages - 1;
+		dispatch(updatePagination(prevPage));
+	};
+
+	const goNext = () => {
+		const prevPage = pages + 1;
+		dispatch(updatePagination(prevPage));
+	};
+
+	return (
+		<Grid>
+			{pages !== 0 && (
+				<Button onClick={() => goPrevious()}>Decrement</Button>
+			)}
+			<Button onClick={() => goNext()}>Increment</Button>
+		</Grid>
+	);
+};
+
+export default Pagination;
