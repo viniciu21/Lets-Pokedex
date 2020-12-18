c9823c99bc33d24639d4fa8a200cabf7db0ebdae
viniciu21 <viniciusoliveiras12@hotmail.com.br>
2020-12-18 07:51:30 -0300

    Add a logic of localStorage permanence

---
+++ src/components/CardPoke/index.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { makePokemonStared, setStaredPokemons } from "../../store/actions/pokedex";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import {
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Button,
+	Typography,
+	Paper,
+	Chip,
+} from "@material-ui/core";
+import StarRateSharpIcon from "@material-ui/icons/StarRateSharp";
+
+import { Pokemon } from "../../store/types/pokedex";
+import { ApplicationState } from "../../store";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			flexGrow: 1,
+		},
+		paper: {
+			border: "10",
+		},
+		image: {},
+	})
+);
+
+interface CardPokeProps {
+	pokemon: Pokemon;
+}
+
+const CardPoke: React.FC<CardPokeProps> = ({ pokemon }) => {
+	const dispatch = useDispatch();
+	const pokedexState = useSelector<
+		ApplicationState,
+		ApplicationState["pokedexReducer"]
+	>((state) => state.pokedexReducer);
+
+	const classes = useStyles();
+
+	const handleStared = () => {
+		const pokemonStared:Pokemon = {...pokemon, stared: true};
+		let existData = localStorage.getItem("pokemonsStared");
+		let pokemonToLocalStorage = existData ? [...JSON.parse(existData)] : [];
+		!(pokemonToLocalStorage.find(poke => poke.id === pokemonStared.id)) && pokemonToLocalStorage.push(pokemonStared);
+		localStorage.setItem("pokemonsStared", JSON.stringify(pokemonToLocalStorage));
+
+		const pokemonsUpdated: Pokemon[] = pokedexState.pokemons.map((poke) => {
+			if (poke.id === pokemonStared.id) return pokemonStared;
+			return poke;
+		});
+		const pokemonsStaredReducer: Pokemon[] = pokemonsUpdated.filter(poke => poke.stared === true);
+		dispatch(setStaredPokemons(pokemonsStaredReducer));
+		dispatch(makePokemonStared(pokemonsUpdated));
+	};
+
+	const handleUnStared = () => {
+		const pokemonUnStared:Pokemon = {...pokemon, stared: false};
+		let existData = localStorage.getItem("pokemonsStared");
+		let pokemonToLocalStorage = existData ? [...JSON.parse(existData)] : [];
+		const pokemonsFiltred = pokemonToLocalStorage.filter(poke => poke.id !== pokemonUnStared.id);
+		localStorage.setItem("pokemonsStared", JSON.stringify(pokemonsFiltred));
+		const pokemonsUpdated: Pokemon[] = pokedexState.pokemons.map((poke) => {
+			if (poke.id === pokemon.id) return { ...poke, stared: false };
+			return poke;
+		});
+		dispatch(setStaredPokemons(pokemonsUpdated));
+		dispatch(makePokemonStared(pokemonsUpdated));
+	};
+
+	return (
+		<Paper className={classes.paper}>
+			<Card className={classes.root} elevation={5}>
+				<CardActionArea>
+					<CardMedia
+						image={pokemon.imgUrl}
+						component="img"
+						className={classes.image}
+						alt={pokemon.name}
+						title={pokemon.name}
+						height="250"
+					/>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="h2">
+							{`Pokemon ${pokemon.id} - ${pokemon.name}`}
+						</Typography>
+						<Typography variant="body2" color="textSecondary" component="h6">
+							This is {pokemon.name}, he is{" "}
+							{pokemon.types.map((type) => (
+								<Chip label={type} key={type} color="secondary" />
+							))}{" "}
+							types, he is so friendly!!!!.
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+				<CardActions>
+					<Button size="small" color="primary" onClick={handleStared}>
+						Favorite
+					</Button>
+					<Button size="small" color="primary" onClick={handleUnStared}>
+						UnFavorite
+					</Button>
+					<div className={classes.root} />
+					{pokemon.stared && <StarRateSharpIcon />}
+				</CardActions>
+			</Card>
+		</Paper>
+	);
+};
+
+export default CardPoke;
---
+++ src/components/Search/index.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import {
+	Avatar,
+	Grid,
+	IconButton,
+	InputBase,
+	Paper,
+	Typography,
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { ApplicationState } from "../../store/index";
+import _ from "lodash";
+import pokedex from "../../assets/pokedex.png";
+import { setFiltredPokemons } from "../../store/actions/pokedex";
+import pokebolaPequena from "../../assets/pokebolaPequena.png";
+import Pagination from "../Pagination/index";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		inputArea: {
+			paddingTop: theme.spacing(2),
+		},
+		paperArea: {
+			padding: "2px 4px",
+			display: "flex",
+			alignItems: "center",
+		},
+		iconButton: {
+			padding: theme.spacing(2),
+		},
+		pokebol: {
+			paddingRight: theme.spacing(1),
+		},
+		title: {
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+		},
+		large: {
+			width: theme.spacing(7),
+			height: theme.spacing(7),
+		},
+	})
+);
+
+interface SearchProps {
+	stared?: boolean;
+}
+
+const Search: React.FC<SearchProps> = ({ stared }) => {
+	const classes = useStyles();
+	const [valueInput, setValueInput] = useState<string>("");
+	const dispatch = useDispatch();
+
+	const { pokemons, staredPokemons } = useSelector<
+		ApplicationState,
+		ApplicationState["pokedexReducer"]
+	>((state) => state.pokedexReducer);
+
+	const handleFilterPokemons = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		setValueInput(e.target.value);
+		if (stared) {
+			const pokemonsFiltratedStar = staredPokemons.filter((pokemon) =>
+				_.includes(pokemon.name, e.target.value)
+			);
+			dispatch(setFiltredPokemons(pokemonsFiltratedStar));
+		} else {
+			const pokemonsFiltrated = pokemons.filter((pokemon) =>
+				_.includes(pokemon.name, e.target.value)
+			);
+			dispatch(setFiltredPokemons(pokemonsFiltrated));
+		}
+	};
+
+	return (
+		<Grid
+			container
+			justify="center"
+			className={classes.inputArea}
+			alignItems="center"
+			direction="column"
+		>
+			<div className={classes.title}>
+				<Typography component="h4" variant="h4" color="inherit">
+					PokeDex
+				</Typography>
+				<Avatar src={pokedex} className={classes.large} />
+			</div>
+			<Paper className={classes.paperArea}>
+				<Avatar src={pokebolaPequena} className={classes.pokebol} />
+				<InputBase
+					placeholder="Search your best pokemon"
+					inputProps={{ "aria-label": "Search your best pokemon" }}
+					value={valueInput}
+					onChange={(e) => handleFilterPokemons(e)}
+				/>
+				<IconButton className={classes.iconButton}>
+					<SearchIcon />
+				</IconButton>
+				{!stared && <Pagination />}
+			</Paper>
+		</Grid>
+	);
+};
+
+export default Search;
